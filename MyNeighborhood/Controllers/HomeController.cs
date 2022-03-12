6ea70507718eef13303066d1e0fbd672202eef67
Pavlina N <p_uzunova@abv.bg>
2022-03-12 18:58:35 +0200

    Login, logout and other views customized

--- MyNeighborhood/Controllers/HomeController.cs
+++ MyNeighborhood/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace MyNeighborhood.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -16,7 +16,7 @@ namespace MyNeighborhood.Controllers
 
         public IActionResult Index()
         {
-            ViewData[MessageConstant.SuccessMessage] = "Урра, успя нещо да тръгне!!!";
+            ViewData[MessageConstant.SuccessMessage] = "Урра,нещо тръгна!!!";
 
             return View();
         }
