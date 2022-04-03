50d96c850011d685ce39a3c7d99f79432947cc6a
Pavlina N <p_uzunova@abv.bg>
2022-04-03 12:52:09 +0300

    Api added

---
+++ MyNeighborhood.Api/Extensions/ApiServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNeighborhood.Infrastructure.Data;
+using MyNeighborhood.Infrastructure.Data.Repositories;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ApiServiceCollectionExtension
+    {
+
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            
+
+            return services;
+
+        }
+
+    }
+}
